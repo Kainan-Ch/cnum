7ae4d192e0ae36dbde2e1e4238a50e2bbd2750f5
marcos.chiabai <marcos.chiabai@estudante.ifes.edu.br>
2025-08-01 19:25:31 +0000

    aula1

---
+++ Semana-1/aula1.py
@@ -0,0 +1,37 @@
+#a.x² +b.x +c"
+"""
+Raiz de Equação Quadrática 
+"""
+import math
+
+def zeros(a,b,c):
+
+    delta = b**2 - (4*a*c) 
+    if delta < 0:
+        return None
+    elif delta == 0:
+        x = -b/(2*a)
+        return[x,x]
+    else:
+        x1 = (-b + math.sqrt(delta))/(2*a)
+        x2 = (-b - delta**0.5)/(2*a)
+        return[x1,x2]
+
+def main():
+    x= zeros(1,-5,6)
+    print(x)
+if __name__ =="__main__":
+    main()
+# se a for igual 0 a solução -c/b    
+"""
+if a == 0:
+    x = -c/b
+    print(x)
+"""    
+
+
+    
+
+    
+    
+
