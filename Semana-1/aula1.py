de14e68c8c90c4ab00b296795e2a7196e195158a
marcos.chiabai <marcos.chiabai@estudante.ifes.edu.br>
2025-08-07 18:07:00 +0000

    Correção do exercicio

--- Semana-1/aula1.py
+++ Semana-1/aula1.py
@@ -3,8 +3,14 @@
 Raiz de Equação Quadrática 
 """
 import math
-
+    
 def zeros(a,b,c):
+# se a for igual 0 a solução -c/b   
+    if a == 0:
+        if b == 0:
+            raise ValueError("A equação não possui solução")
+        else:
+            return -c/b
 
     delta = b**2 - (4*a*c) 
     if delta < 0:
@@ -22,12 +28,8 @@ def main():
     print(x)
 if __name__ =="__main__":
     main()
-# se a for igual 0 a solução -c/b    
-"""
-if a == 0:
-    x = -c/b
-    print(x)
-"""    
+
+   
 
 
     
