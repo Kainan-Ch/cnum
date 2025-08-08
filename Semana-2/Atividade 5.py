98c2eb77a8358b713dfed4b51f692d448d2dd250
marcos.chiabai <marcos.chiabai@estudante.ifes.edu.br>
2025-08-08 18:50:32 +0000

    atividade realizadas

--- Semana-2/Atividade 5.py
+++ Semana-2/Atividade 5.py
@@ -17,3 +17,62 @@ multiplicado por:
 [14, 15, 16],
 [17, 18, 19] ]
 """
+import numpy as np
+
+
+def is_perfect(n: int) -> bool:
+    if n < 1:
+        return False
+
+    sum_divisors = 0
+    for i in range(1, n):
+        if n % i == 0:
+            sum_divisors += i
+
+    return sum_divisors == n
+
+def factorial(n: int) -> int:
+    if n < 0:
+        raise ValueError("O número deve ser não negativo.")
+
+    result = 1
+    i = n
+    while i > 1:
+        result *= i
+        i -= 1
+
+    return result
+
+def is_prime(n: int) -> bool:
+    if n < 2:
+        return False
+
+    for i in range(2, int(n/2)):
+        if n % i == 0:
+            return False
+
+    return True
+
+def sum_of_digits(n: int) -> int:
+    if n < 0:
+        raise ValueError("O número deve ser não negativo.")
+    sum = 0
+    while n > 0:
+        sum += n % 10
+        n //= 10
+    return sum
+
+A = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
+
+B = np.array([[10, 11, 12],
+              [13, 14, 15],
+              [16, 17, 18]])
+
+C = A @ B
+
+
+
+if __name__ == "__main__":
+    main()
